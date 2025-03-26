571f19484cda00957ac6732e8c4dd786aeb85045
Ganesh-Ekhe <ganeshekhe8@gmail.com>
2025-03-26 13:16:05 +0530

    Fix: Allow Render host

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
-    port: process.env.PORT || 4173 // Render च्या PORT वर रन करेल
+    port: 4173,
+    strictPort: true,
+    hmr: {
+      port: 443,
+    }
+  },
+  preview: {
+    allowedHosts: ['edusity-g7yq.onrender.com']
   }
 })
